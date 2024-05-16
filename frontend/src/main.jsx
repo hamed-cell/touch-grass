668f049a6ebb2c9208118f47231e3c543ae29fcc
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-05-16 11:49:48 +0200

    HomePage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,17 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+// import { Link } from "react-router-dom";
+
+export default function HomePage() {
+  return (
+    <div className="HomePage">
+      <h1>Touch Grass</h1>
+      {/* <Link to=""> */}
+      <button type="button">Go find grass!</button>
+      {/* </Link> */}
+    </div>
+  );
+}
